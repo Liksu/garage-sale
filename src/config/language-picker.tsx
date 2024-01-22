a062b45cd6009b4fcbd0b8f85b4f55eb98bd085e
Peter Bortchagovsky <github@Liksu.kiev.ua>
2024-01-22 19:41:00 +0100

    Disable language picker

--- src/config/language-picker.tsx
+++ src/config/language-picker.tsx
@@ -1,16 +1,26 @@
 import { ReactElement, useContext } from 'react'
 import { ConfigContext } from '../helpers/config-provider'
-import { Image } from '@mantine/core'
+import { ActionIcon, Image } from '@mantine/core'
 
 export default function LanguagePicker(): ReactElement {
     const { languages } = useContext(ConfigContext)
-
-    return (<>
-        {languages.map((language) => (
-            <Image key={language}
-                   src={`https://unpkg.com/language-icons/icons/${language.toLowerCase()}.svg`}
-                   w={24}
-                   alt={language} />
-        ))}
-    </>)
+    
+    if (languages.length <= 1) return (<></>)
+    
+    return (<></>)
+    
+    //
+    // return (
+    //     <ActionIcon>
+    //     </ActionIcon>
+    // )
+    //
+    // return (<>
+    //     {languages.map((language) => (
+    //         <Image key={language}
+    //                src={`https://unpkg.com/language-icons/icons/${language.toLowerCase()}.svg`}
+    //                w={24}
+    //                alt={language} />
+    //     ))}
+    // </>)
 }
